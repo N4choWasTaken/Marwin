411ed3092d8d746362b68b9f37603592b3c311a2
N4choWasTaken <thierrygmuer@outlook.com>
2022-10-21 03:37:46 +0200

    modified: bot.py / bot starts and listens to client events

--- bot.py
+++ bot.py
@@ -14,3 +14,25 @@ async def send_message(message, user_message, is_private):
 def run_discord_bot():
     load_dotenv()
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents = intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} in {channel} said: {user_message}')
+
+        await send_message(message, user_message, is_private=False)
+
+    client.run(DISCORD_TOKEN)
